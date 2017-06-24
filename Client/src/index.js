e0abcc3f813756e703bd27509b2abfc49c3c7a59
biskazz <biser.p.atanasov@gmail.com>
2017-06-24 19:16:04 +0300

    Added bootstrap

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/paper/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
