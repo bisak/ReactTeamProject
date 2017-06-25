8fd351b486a4b36fecbdc98a76c36708c8e57a84
s7ankoff <ico_stankov@abv.bg>
2017-06-25 19:35:03 +0300

    bug

--- Client/src/routes/routes.js
+++ Client/src/routes/routes.js
@@ -3,12 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import HomeComponent from '../components/HomeComponent'
 import NotFoundComponent from '../components/NotFoundComponent'
-<<<<<<< HEAD
-import ProductsComponent from '../components/ProductsComponent'
-import AllUsersComponent from '../components/AllUsersComponent'
-=======
 import AllProductsComponent from '../components/AllProductsComponent'
->>>>>>> f95a4a813f843e340bb9e425beca0213e4919049
 import ProfileComponnet from '../components/ProfileComponent'
 import AddProductComponent from '../components/AddProductComponent'
 import SingleProductComponent from '../components/SingleProductComponent'
@@ -21,15 +16,6 @@ import BanUserComponent from '../components/BanUserComponent'
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={HomeComponent} />
-<<<<<<< HEAD
-    <Route exact path='/login' component={LoginComponent} />
-    <Route exact path='/register' component={RegisterComponent} />
-    <Route exact path='/products' component={ProductsComponent} />
-    <Route exact path='/product/:id' component={SingleProductComponent} />
-    <Route exact path='/admin/All' component={AllUsersComponent} />
-    <Route exact path='/profile/:id' component={ProfileComponnet} />
-    <Route exact path='/admin/addproduct' component={AddProductComponent} />
-=======
     <Route path='/login' component={LoginComponent} />
     <Route path='/register' component={RegisterComponent} />
     <Route path='/products' component={AllProductsComponent} />
@@ -42,7 +28,6 @@ const Routes = () => (
     <Route path='/admin/all-admins' component={AllAdminsComponent} />
     <Route path='/admin/ban-user' component={BanUserComponent} />
 
->>>>>>> f95a4a813f843e340bb9e425beca0213e4919049
     <Route component={NotFoundComponent} />
   </Switch>
 )
