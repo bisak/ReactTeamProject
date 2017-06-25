c4ff40f9b48e9feed153ecd65ecbeed5d62e09af
s7ankoff <ico_stankov@abv.bg>
2017-06-25 17:32:29 +0300

    added ProductsView

---
+++ Client/src/components/ProductsView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ProductsView extends Component {
+    render() {
+
+        return (
+            <div>
+              <h2>Produtcs....</h2> 
+               
+            </div>
+        )
+    }
+}
+export default ProductsView
--- Client/src/routes/routes.js
+++ Client/src/routes/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomeComponent from '../components/HomeComponent'
 import NotFoundPage from '../components/NotFoundPage'
+import ProductsView from '../components/ProductsView'
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={HomeComponent} />
+    <Route exact path='/ProductsView' component={ProductsView} />
 
     <Route component={NotFoundPage} />
   </Switch>
